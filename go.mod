01ec323efc841de706f121e14eff185ba1755d48
thort <thort@compass-vm>
2025-01-22 17:51:59 +0100

    add failing test

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tobiashort/jwt-testcases
+
+go 1.23.4
--- jwt_test.go
+++ jwt_test.go
@@ -13,3 +13,19 @@ func TestDecodeJWT(t *testing.T) {
 		}
 	}
 }
+
+func TestEncodeJWT(t *testing.T) {
+	encodedJWTs := []string{
+		"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c",
+	}
+	for _, encodedJWT := range encodedJWTs {
+		decodedJWT, err := DecodedJWT(encodedJWT)
+		if err != nil {
+			t.Error(encodedJWT, err)
+		}
+		reencodedJWT := decodedJWT.Encode()
+		if reencodedJWT != encodedJWT {
+			t.Errorf("\n   %s\n-> %s", encodedJWT, reencodedJWT)
+		}
+	}
+}
